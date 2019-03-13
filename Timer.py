3abb8915a38a8f78e6e6fd1c4281348975ed375e
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-03-14 06:15:02 +0900

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLCDNumber
+from PyQt5.QtCore import QTimer, QDateTime
+
+class Timer(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.initUI()
+  
+  def initUI(self):
+    timer = QTimer(self)
+    timer.timeout.connect(self.updtTime)
+    self.timeDisplay = QLCDNumber(self)
+    self.timeDisplay.setSegmentStyle(QLCDNumber.Filled)
+    self.timeDisplay.setDigitCount(8)
+    self.updtTime()
+    timer.start(1000)
+
+    self.setGeometry(300, 300, 500, 200)
+    self.setWindowTitle('Timer')
+    self.show()
+
+  def updtTime(self):
+    currentTime = QDateTime.currentDateTime().toString('hh:mm:ss')
+    self.timeDisplay.display(currentTime)
+
+if __name__ == '__main__':
+  app = QApplication(sys.argv)
+  ex = Timer()
+  sys.exit(app.exec_())
