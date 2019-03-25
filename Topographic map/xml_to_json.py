4d319e8b0c377b136c9e50ab0b9745f617113549
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-03-25 09:47:00 +0900

    Update xml_to_json.py

--- Topographic map/xml_to_json.py
+++ Topographic map/xml_to_json.py
@@ -3,40 +3,60 @@ import os
 import sys
 import json
 
+# 標高データを抜き出す正規表現
 ptn_elev = re.compile(r"(.*),(.*)")
+# データの開始地点を抜き出す正規表現
 ptn_sp = re.compile(r"<gml:startPoint>(.*) (.*)</gml:startPoint>")
+# ファイル名を抜き出す正規表現
 ptn_file = re.compile(r"FG-GML-(.*)-(.*)-(.*)-.*-.*.xml")
-
+# ファイルが保存してあるディレクトリ
 root = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 def convert(filename):
+  # 標高データを入れる配列
   elevations = []
+  # データをjsonファイルにするために使う辞書型配列
   meta = {}
+  # ファイルを開く utf-8_sigの部分は各々の環境によって変わるかもしれません
   with open(filename, "r", encoding = "utf-8_sig") as f:
     while True:
+      # ファイうrを一行ずつ読み込む
       line = f.readline()
+      # 標高のパターンにマッチするか探す
       match_elev = ptn_elev.search(line)
+      # マッチしたら処理
       if match_elev:
+        # データのタイプとデータに分割
         typ, data = line.split(",")
+        # データのタイプがデータなしや海水面ならNanに
         if typ in (u"データなし", u"海水面"):
           data = u"Nan"
+        # 標高データをfloat型にして追加
         elevations.append(float(data)) 
+      # 開始地点のパターンとマッチするか探す
       match_sp = ptn_sp.search(line)
+      # マッチたら処理
       if match_sp:
+        # 開始地点を保存しておく
         meta["sp"] = (int(match_sp.group(1)), int(match_sp.group(2)))
         break
+  # 配列の大きさ
   shape = (225, 150)
   meta["shape"] = shape
   meta["sp"] = meta["sp"][1] * 225 + meta["sp"][0]
+  # ファイル名とマッチしているか
   match_file = ptn_file.match(os.path.basename(filename))
+  # ファイルの保存先のディレクトリ
   dir_dest = root + "/" + root[-20:]+"_json/"
+  # ファイル名
   dest_filebase = "{}".format(match_file.group(3))
+  # startPointとelevationsというキーにそれぞれmeta['sp'], elevationsを要素に
   data = {"startPoint": meta["sp"], "elevations": elevations}
   json.dump(
     data,
     open(dir_dest + "{}.json".format(dest_filebase), "w")
   )
-
+# root: 現在のディレクトリ dirs: root下に存在するディレクトリ files: root下に存在するファイル
 for roots, _, files in os.walk(root + "/"):
   for f in files:
     file_name = os.path.join(roots, f)
