96597603f02e0b2260d312e6134e28ae60a0b7fd
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-08-26 14:13:00 +0900

    Create __init__.py

---
+++ DBViewer/DBViewer/__init__.py
@@ -0,0 +1,4 @@
+from .DBViewerUI import MainUI, DBListUI
+from .DBViewerUIFunc import MyTree, Notifier, Thread
+from .DBViewerDirSetting import DirSetting
+from .myfunc import basename, adjustSep, inExtension
